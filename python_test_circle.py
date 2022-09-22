3a61754fff0dc951928af8c479a4af63dbc3c1fd
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-09-22 21:22:38 +0000

    Program that calculates and displays area and perimeter of a circle with a radius of 15mm

---
+++ python_test_circle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Spencer.Scarlett
+# Created on: Sept 22, 2022
+# This program calculates and displays the area and perimeter of a
+# circle with radius 15 mm.
+import math
+
+
+def main():
+    print("Area = {} mm^2".format(math.pi * 15**2))
+    print("Perimeter = {} mm".format(2 * math.pi * 15))
+
+
+if __name__ == "__main__":
+    main()
